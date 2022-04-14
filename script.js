e0c2e40b1487b33747b8b74d0cbd0e0d78a2c2b9
AliSaghaei <79021514+AliSaghaei@users.noreply.github.com>
2022-04-14 11:52:38 -0700

    level 200 completed

--- script.js
+++ script.js
@@ -1,49 +1,61 @@
 // API URLs
 const API_URL = "https://api.tvmaze.com/shows/82/episodes";
 
-// const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280/";
-// const SEARCH_URL =
-//   "https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=7c2ed6e1f883aaa7f6a2246351725c9a&query='";
-
 // Grabbing the Elements
 const form = document.getElementById("form"),
   search = document.getElementById("search"),
   main = document.getElementById("main");
 
+// Fetched data
+let data;
+let searchTerm = "";
 // Get Movies
 getMovies(API_URL);
 async function getMovies(url) {
-  const res = await fetch(url);
-  const data = await res.json();
+  data = await fetch(url).then((res) => res.json());
   displayMovies(data);
-  console.log(data);
 }
 
 // Display Movies
-function displayMovies(movies) {
+async function displayMovies(movies) {
   main.innerHTML = "";
-  movies.forEach((movie) => {
-    let { name, image, season, number, summary } = movie;
-    if (season < 10) {
-      season = `0${season}`;
-    }
-    if (number < 10) {
-      number = `0${number}`;
-    }
-    const movieElement = document.createElement("div");
-    movieElement.classList.add("movie");
-    movieElement.innerHTML = `
+
+  movies
+    .filter(
+      (country) =>
+        country.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        country.summary.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .forEach((movie) => {
+      let { name, image, season, number, summary } = movie;
+      if (season < 10) {
+        season = `0${season}`;
+      }
+      if (number < 10) {
+        number = `0${number}`;
+      }
+      const movieElement = document.createElement("div");
+      movieElement.classList.add("movie");
+      movieElement.innerHTML = `
     <img src='${image.medium}' alt='${name}' />
     <div class="movie-info">
     <h3>${name}</h3>
     <span>S${season}E${number}</span>
     <div class='summary'>
-    <h3>Overview</h3>
+    <h3>Summary</h3>
     ${summary}
     </div>
     </div>
     `;
 
-    main.appendChild(movieElement);
-  });
+      main.appendChild(movieElement);
+    });
 }
+
+// Search Form
+
+search.addEventListener("input", (e) => {
+  searchTerm = e.target.value;
+
+  displayMovies(data);
+});
